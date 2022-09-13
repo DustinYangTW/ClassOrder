d51a0d844e1d6f3ffdcc44f7161b1a2a36a71f6b
Dustin <yangkschool@gmail.com>
2022-09-13 12:46:34 +0800

    first commit

---
+++ HW7Project/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HW7Project.Models;
+
+namespace HW7Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        HW7ProjectContext db = new HW7ProjectContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var products = db.Products.Where(p => p.Discontinued == false).ToList();
+
+            return View(products);
+        }
+
+
+        public ActionResult MyCart()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(VMLogin vMLogin)
+        {
+
+            //select * from Members where account=@account and password=@password
+            string pw = Members.getHashPassword(vMLogin.Password);
+            var member = db.Members.Where(m => m.Account == vMLogin.Account && m.Password == pw).FirstOrDefault();
+
+            if (member == null)
+            {
+                ViewBag.ErrMsg = "帳號或密碼有錯!!";
+                return View(vMLogin);
+            }
+
+            Session["member"] = member;
+            Session["memberID"] = member.MemberID;
+            return RedirectToAction("ProductList");
+
+
+        }
+
+
+        [LoginCheck]
+        public ActionResult Logout()
+        {
+
+            Session["member"] = null;
+            Session["memberID"] = null;
+            return RedirectToAction("ProductList", "Home");
+        }
+
+        [LoginCheckMemebers]
+        public ActionResult CheckList()
+        {
+            int membersID = Int16.Parse((Session["memberID"].ToString()));
+            var list = db.Orders.Where(p => p.MemberID == membersID).ToList();
+            return View(list);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+**【作品展示】**</br>
+**<a href="https://www.classk2022store.somee.com/">點我觀看</a></br>**
+
+**`測試帳號 : shaio`**   </br>
+**`測試密碼 : 12345678`** </br> </br>
+![image](https://user-images.githubusercontent.com/106080221/189526517-5ec0e7ad-b109-45e9-a4d8-0a264d5bd83b.png)
+</br>
+
+
+
+**【Local Storage】**</br></br>
+利用JS與Jquery把資料暫存在Local Storage，讓使用者關閉瀏覽器後，下次在打開還會看到以前的選項
+
+![image](https://user-images.githubusercontent.com/106080221/189554348-78aaaf97-0f24-431d-854a-fc361b365b16.png)
+</br></br>
+
+**【SQL預存程序】**</br>
+透過傳入的值，整理成List後，都給SQL處理，並存入資料庫內</br></br>
+![image](https://user-images.githubusercontent.com/106080221/189559866-5bb364b1-2cdb-4c89-9b07-997980001e3f.png)
+![image](https://user-images.githubusercontent.com/106080221/189559701-3c0e42a1-995d-4c62-877b-991a24295206.png)
